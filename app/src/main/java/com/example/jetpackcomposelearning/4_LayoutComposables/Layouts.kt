b856148db277d1fd10deb36e31b59ed3f1ccb1ff
CodeBase-Amit <sharma.amit262004@gmail.com>
2026-01-02 00:43:42 +0530

    Learned about Types of rows and columns

--- app/src/main/java/com/example/jetpackcomposelearning/4_LayoutComposables/Layouts.kt
+++ app/src/main/java/com/example/jetpackcomposelearning/4_LayoutComposables/Layouts.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -90,7 +91,7 @@ fun ConstraintLayoutExample() {
     Column {
         ConstraintLayout(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .height(100.dp)
                 .background(color = Color.LightGray)
         ) {
